8b09be7bf8ae336583ff8adabdb863993ad1d1fd
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-01-04 11:54:53 -0800

    Created shell Script

--- backend.sh
+++ backend.sh
@@ -17,4 +17,4 @@ systemctl enable backend
 systemctl restart backend
 
 dnf install mysql -y
-mysql -h 172.31.41.84 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 54.227.37.36 -uroot -pExpenseApp@1 < /app/schema/backend.sql
