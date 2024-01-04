a8d65e3d883bd2b61b0fd05cbb633048c2b412b3
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-01-04 13:48:49 -0800

    Created shell Script

--- backend.sh
+++ backend.sh
@@ -1,40 +1,41 @@
-echo DISABLING NODEJS VERSION
+
+echo -e "\e[31mDISABLING NODEJS VERSION\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
-echo INSTALLING NODEJS VERSION
+echo echo -e "\e[31mINSTALLING NODEJS VERSION\e[0m"
 dnf install nodejs -y
 
-echo BACKEND SERVICE CONFIGURATION
+echo -e "\e[31mBACKEND SERVICE CONFIGURATION\e[0m"
 cp backend.service /etc/systemd/system/backend.service
 
-echo ADDING APPLICATION USER
+echo -e "\e[31mADDING APPLICATION USER\e[0m"
 useradd expense
 
-echo DELETING EXITING APP CONTENT
+echo -e "\e[31mDELETING EXITING APP CONTENT\e[0m"
 rm -rf /app
 
-echo CREATING APPLICATION DIRECTORY
+echo -e "\e[31mCREATING APPLICATION DIRECTORY\e[0m"
 mkdir /app
 
-echo DOWNLOADING APPLICATION CONTENT
+echo -e "\e[31mDOWNLOADING APPLICATION CONTENT\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 
 cd /app
 
-echo EXTRACTING APPLICATION CONTENT
+echo -e "\e[31mEXTRACTING APPLICATION CONTENT\e[0m"
 unzip /tmp/backend.zip
 
-echo DOWNLOADING APPLICATION DEPENDENCIES
+echo -e "\e[31mDOWNLOADING APPLICATION DEPENDENCIES\e[0m"
 npm install
 
-echo RELOADING SYSTEM D ENABLE AND BACKEND SERVICE
+echo -e "\e[31mRELOADING SYSTEM D ENABLE AND BACKEND SERVICE\e[0m"
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
 
-echo INSTALLING MYSQL CLIENT
+echo -e "\e[31mINSTALLING MYSQL CLIENT\e[0m"
 dnf install mysql -y
 
-echo LOADING SCHEMA
+echo -e "\e[31mLOADING SCHEMA\e[0m"
 mysql -h mysql-dev.sidevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
