acc6a27e44268f26df777198b72e0b17ac140144
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-01-04 11:26:54 -0800

    Created shell Script

--- backend.sh
+++ backend.sh
@@ -6,7 +6,6 @@ mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
-cd /app
 npm install
 cp backend.service /etc/systemd/system/backend.service
 systemctl daemon-reload
