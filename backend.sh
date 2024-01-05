2406067699c19ec68138487b84e516583ee75fe8
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-01-05 11:36:11 -0800

    Shell Code

--- backend.sh
+++ backend.sh
@@ -2,49 +2,49 @@ MYSQL_PASSWORD=$1
 log_file=/tmp/expense.log
 
 head() {
-  Head"$1"
+  echo "\e[31m$1\e[0m"
 }
 
 
-Head"DISABLING NODEJS VERSION"
+Head "DISABLING NODEJS VERSION"
 dnf module disable nodejs -y &>>$log_file
 
-Head"ENABLING NODEJS VERSION"
+Head "ENABLING NODEJS VERSION"
 dnf module enable nodejs:18 -y &>>$log_file
 
-Head"INSTALLING NODEJS VERSION"
+Head "INSTALLING NODEJS VERSION"
 dnf install nodejs -y &>>$log_file
 
-Head"BACKEND SERVICE CONFIGURATION"
+Head "BACKEND SERVICE CONFIGURATION"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
-Head"ADDING APPLICATION USER"
+Head "ADDING APPLICATION USER"
 useradd expense &>>$log_file
 
-Head"DELETING EXITING APP CONTENT"
+Head "DELETING EXITING APP CONTENT"
 rm -rf /app &>>$log_file
 
 Head"CREATING APPLICATION DIRECTORY"
 mkdir /app &>>$log_file
 
-Head"DOWNLOADING APPLICATION CONTENT"
+Head "DOWNLOADING APPLICATION CONTENT"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>/tmp/expense.log
 
 cd /app &>>$log_file
 
-Head"EXTRACTING APPLICATION CONTENT"
+Head "EXTRACTING APPLICATION CONTENT"
 unzip /tmp/backend.zip &>>$log_file
 
-Head"DOWNLOADING APPLICATION DEPENDENCIES"
+Head "DOWNLOADING APPLICATION DEPENDENCIES"
 npm install &>>$log_file
 
-Head"RELOADING SYSTEM D ENABLE AND BACKEND SERVICE"
+Head "RELOADING SYSTEM D ENABLE AND BACKEND SERVICE"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
 
-Head"INSTALLING MYSQL CLIENT"
+Head "INSTALLING MYSQL CLIENT"
 dnf install mysql -y &>>$log_file
 
-Head"LOADING SCHEMA"
+Head "LOADING SCHEMA"
 mysql -h mysql-dev.sidevops.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
