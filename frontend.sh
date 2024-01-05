89f7c72b01029c78707521563b99cc83220103a4
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-01-05 13:15:42 -0800

    Shell Code

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,39 @@
-dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl restart nginx
+MYSQL_PASSWORD=$1
+log_file=/tmp/expense.log
 
+Head() {
+  echo -e "\e[33m$1\e[0m"
+}
+
+Head "INSTALLING NGINX"
+dnf install nginx -y &>>$log_file
+echo $?
+
+Head "Copy Expense Config File"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $?
+
+Head "Enabling NGINX"
+systemctl enable nginx &>>$log_file
+echo $?
+
+Head "STARTING NGINX"
+systemctl start nginx
+echo $?
+
+Head "DELETING EXISTING CONTENT"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
+
+Head "DOWNLOADING APPLICATION CODE"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo $?
+
+cd /usr/share/nginx/html
+
+
+unzip /tmp/frontend.zip &>>$log_file
+echo $?
+systemctl restart nginx &>>$log_file
+
+echo $?
