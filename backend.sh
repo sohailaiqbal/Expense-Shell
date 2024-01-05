fdc127ae6ff03caa6ff6accefe5a69412fea0c82
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-01-05 11:26:40 -0800

    Shell Code

--- backend.sh
+++ backend.sh
@@ -1,44 +1,50 @@
 MYSQL_PASSWORD=$1
 log_file=/tmp/expense.log
-echo -e "\e[36mDISABLING NODEJS VERSION\e[0m"
+
+head() {
+  Head"$1"
+}
+
+
+Head"DISABLING NODEJS VERSION"
 dnf module disable nodejs -y &>>$log_file
 
-echo -e "\e[36mENABLING NODEJS VERSION\e[0m"
+Head"ENABLING NODEJS VERSION"
 dnf module enable nodejs:18 -y &>>$log_file
 
-echo -e "\e[36mINSTALLING NODEJS VERSION\e[0m"
+Head"INSTALLING NODEJS VERSION"
 dnf install nodejs -y &>>$log_file
 
-echo -e "\e[36mBACKEND SERVICE CONFIGURATION\e[0m"
+Head"BACKEND SERVICE CONFIGURATION"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
-echo -e "\e[36mADDING APPLICATION USER\e[0m"
+Head"ADDING APPLICATION USER"
 useradd expense &>>$log_file
 
-echo -e "\e[36mDELETING EXITING APP CONTENT\e[0m"
+Head"DELETING EXITING APP CONTENT"
 rm -rf /app &>>$log_file
 
-echo -e "\e[36mCREATING APPLICATION DIRECTORY\e[0m"
+Head"CREATING APPLICATION DIRECTORY"
 mkdir /app &>>$log_file
 
-echo -e "\e[36mDOWNLOADING APPLICATION CONTENT\e[0m"
+Head"DOWNLOADING APPLICATION CONTENT"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>/tmp/expense.log
 
 cd /app &>>$log_file
 
-echo -e "\e[36mEXTRACTING APPLICATION CONTENT\e[0m"
+Head"EXTRACTING APPLICATION CONTENT"
 unzip /tmp/backend.zip &>>$log_file
 
-echo -e "\e[36mDOWNLOADING APPLICATION DEPENDENCIES\e[0m"
+Head"DOWNLOADING APPLICATION DEPENDENCIES"
 npm install &>>$log_file
 
-echo -e "\e[36mRELOADING SYSTEM D ENABLE AND BACKEND SERVICE\e[0m"
+Head"RELOADING SYSTEM D ENABLE AND BACKEND SERVICE"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
 
-echo -e "\e[36mINSTALLING MYSQL CLIENT\e[0m"
+Head"INSTALLING MYSQL CLIENT"
 dnf install mysql -y &>>$log_file
 
-echo -e "\e[36mLOADING SCHEMA\e[0m"
+Head"LOADING SCHEMA"
 mysql -h mysql-dev.sidevops.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
